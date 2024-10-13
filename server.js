a2444b3d0d49da9f7b281cc2471808cf732ddda9
DianaEri <diana_eriksson@yahoo.com>
2024-10-13 14:59:30 +0100

    Changes

--- server.js
+++ server.js
@@ -13,7 +13,44 @@ app.use(express.json()); // Middleware to parse JSON
 app.get('/api/data', (req, res) => {
   res.json({
     message: 'Hello from Orchid Travels API!',
-    locations: ['Phuket', 'Krabi', 'Bangkok'],
+    hotels: [
+      {
+        name: "Laguna Pearl Retreat",
+        description: [
+          "Nearest beach 100m",
+          "16 pools and spa",
+          "All-inclusive restaurant that offers dietary restrictions options",
+          "Airport Pick-up and Drop-off",
+          "Adapted accommodation for all types of families"
+        ],
+        price_per_person: "21 491kr",
+        family: "Kids friendly",
+        location: "Phuket"
+      },
+      {
+        name: "Phuket Horizon Suites",
+        description: [
+          "Nearest beach 300m",
+          "3 pools",
+          "WiFi is included throughout the hotel",
+          "All-Inclusive at an additional cost"
+        ],
+        price_per_person: "22 150kr/person",
+        location: "Phuket"
+      },
+      {
+        name: "Azure Sands Resort & Spa",
+        description: [
+          "Nearest beach 250m",
+          "5 pools and spa",
+          "WiFi is included throughout the hotel",
+          "Gym",
+          "Environmentally friendly"
+        ],
+        price_per_person: "21 100kr",
+        location: "Phuket"
+      }
+    ]
   });
 });
 
