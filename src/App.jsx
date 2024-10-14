0eab5186b6243969fdb9d5c11bf2416da6799f5e
DianaEri <diana_eriksson@yahoo.com>
2024-10-14 14:44:18 +0100

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faMessage, faPhone, faLocationDot, faPlaneArrival, faUtensils } from '@fortawesome/free-solid-svg-icons';
 import { faYoutube, faSquareInstagram, faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
-import Bild from './assets/Bild.jpg';
 import SearchForm from './SearchForm'; // Import the SearchForm component
 import Footer from './Footer'; // Make sure this import is correct
 import HotelInfoIcons from './HotelInfoIcons'; // Make sure this import is also correct
@@ -11,7 +10,7 @@ import Divider from './Divider'; // Import the Divider component
 import BlackLogo from './assets/Black logo.svg';
 import TripadvisorLogo from './assets/tripadvisor.svg';
 import Header from './Header'; // Import the Header component
-import YellowLine from './YellowLine'; // Import the YellowLine component
+import Banner from './Banner';
 
 const App = () => {
   const [selectedDestination, setSelectedDestination] = useState('City');
@@ -37,12 +36,10 @@ const App = () => {
 
   return (
     <div className="App">
+     {/*Header component*/}
       <Header />
-      <div style={{ width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
-        <img src={Bild} alt="Top banner" style={{ width: '100%', height: 'auto' }} />
-      </div>
+      <Banner />
       <h1 className="center-heading">Where do you want to travel?</h1>
-      <YellowLine/>
       {/* Render the SearchForm component and pass handleSubmit */}
       <SearchForm 
         onSubmit={handleSubmit} 
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Bild from './assets/Bild.jpg';
 
+//Picture styled and made in Figma, shortcut instead of 
+//doing CSS overlays on image. This component only figures in startpage.
+
 const Banner = () => {
   return (
     <div style={{ width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
