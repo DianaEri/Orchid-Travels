086de0215dc521a891562d3019108c7c047edcb0
MadeleneRose <katarinamadelene@hotmail.com>
2024-10-14 17:51:33 +0200

    TextBlock Component

---
+++ src/TextBlock.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+/*
+  Skapar en specifikt stylad textlåda där innehåll, storlek, färg och 
+  andra egenskaper kan anpassas via props för återanvändbarhet. Istället 
+  för att TextBlock alltid visar samma text eller samma stil, kan man ändra 
+  dessa aspekter genom att skicka in olika props när komponenten används. 
+  Koden har anpassats för att uppfylla utseendemålen för "Orchid Travels 
+  "applikationen. Texten anpassas beroende på vad som skickas in som prop "text" 
+  när komponenten renderas.
+*/
+
+const TextBlock = ({ 
+  text, 
+  fontSize = '1.3rem', 
+  color = 'black', 
+  textAlign = 'left', 
+  paddingLeft = '2rem',  
+  paddingRight = '2rem',
+  paddingBottom = '1rem' 
+}) => {
+  const style = {
+    fontSize: fontSize,   
+    color: color,        
+    textAlign: textAlign,   
+    paddingLeft: paddingLeft,  
+    paddingRight: paddingRight, 
+    paddingBottom: paddingBottom, 
+    fontFamily: 'Roboto, sans-serif !important'
+  };
+
+  return <p style={style}>{text}</p>; 
+};
+
+export default TextBlock;
