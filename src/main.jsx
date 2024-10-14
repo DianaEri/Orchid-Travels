0a8c4a83d7027820466b9c8f06654c67aa6f8166
DianaEri <diana_eriksson@yahoo.com>
2024-10-14 13:41:39 +0100

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Import routing components
 import App from './App.jsx';
-import SearchResult from './searchResult.jsx';  // Import your search result page
+import SearchResult from './SearchResult.jsx';  // Import your search result page
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
