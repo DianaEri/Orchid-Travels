2246c54830419a5907ffd7ef1a186ea844bd0165
DianaEri <diana_eriksson@yahoo.com>
2024-10-15 18:57:26 +0100

    Changes

--- src/HotelReviews.jsx
+++ src/HotelReviews.jsx
@@ -1,21 +1,18 @@
-// HotelReviews.jsx
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMessage, faPhone, faLocationDot, faPlaneArrival, faUtensils } from '@fortawesome/free-solid-svg-icons';
 
 const HotelReviews = () => {
   return (
-    <div className="reviewIcons">
-      <div className="reviewList">
-        <div className="boxRow1">
+    <div className="reviewList"> {/* Removed outer div */}
+      <div className="boxRow1">
         <span>From Total price</span>
-        </div>
-        <div className="boxRow2">
-          <FontAwesomeIcon icon={faMessage} />
-          <span>Call us</span>
-          <FontAwesomeIcon icon={faPhone} />
-          <span>Reviews</span>
-        </div>
+      </div>
+      <div className="boxRow2">
+        <FontAwesomeIcon icon={faMessage} />
+        <span>Call us</span>
+        <FontAwesomeIcon icon={faPhone} />
+        <span>Reviews</span>
       </div>
     </div>
   );
