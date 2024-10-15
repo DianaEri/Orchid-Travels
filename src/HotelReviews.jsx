c72f110e3065b92cee6555fdd7978682bb298fb4
DianaEri <diana_eriksson@yahoo.com>
2024-10-15 21:27:01 +0100

    Changes

--- src/HotelReviews.jsx
+++ src/HotelReviews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMessage, faPhone, faLocationDot, faPlaneArrival, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faYelp } from '@fortawesome/free-brands-svg-icons';
 
 const HotelReviews = () => {
   return (
@@ -9,10 +10,20 @@ const HotelReviews = () => {
         <span>From Total price</span>
       </div>
       <div className="reviewRow2">
-        <FontAwesomeIcon icon={faMessage} />
-        <span>Call us</span>
-        <FontAwesomeIcon icon={faPhone} />
-        <span>Reviews</span>
+        <div className="reviewItem">
+          <FontAwesomeIcon icon={faStar} />
+          <span> 4.5/5</span>
+        </div>
+        <div className="reviewItem">
+          <span>Orchid Travels guests</span>
+        </div>
+        <div className="reviewItem">
+          <FontAwesomeIcon icon={faYelp} />
+          <span> 4.7/5</span>
+        </div>
+        <div className="reviewItem">
+        <span>Yelp</span>
+      </div>
       </div>
     </div>
   );
