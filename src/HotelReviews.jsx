b1e99471db98e728109347ac8dbcf036d939273e
DianaEri <diana_eriksson@yahoo.com>
2024-10-16 14:03:36 +0100

    changes

--- src/HotelReviews.jsx
+++ src/HotelReviews.jsx
@@ -3,27 +3,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faAirbnb } from '@fortawesome/free-brands-svg-icons';
 
-const HotelReviews = () => {
+const HotelReviews = ({ totalPrice }) => {
   return (
-    <div className="reviewList"> {/* Removed outer div */}
+    <div className="reviewList">
       <div className="reviewRow1">
-        <span className='bold'>From Total price</span>
+        {/* Display the Total Price */}
+        <span className="bold">
+          From Total Price: {totalPrice !== undefined ? `${totalPrice.toLocaleString('sv-SE')} kr` : 'N/A'}
+        </span>
       </div>
       <div className="reviewRow2">
         <div className="reviewItem">
           <FontAwesomeIcon icon={faStar} />
-          <span className='bold'> 4.5/5</span>
+          <span className="bold"> 4.5/5</span>
         </div>
         <div className="reviewItem">
           <span>Hotel guests</span>
         </div>
         <div className="reviewItem">
           <FontAwesomeIcon icon={faAirbnb} />
-          <span className='bold'> 4.7/5</span>
+          <span className="bold"> 4.7/5</span>
         </div>
         <div className="reviewItem">
-        <span>Airbnb</span>
-      </div>
+          <span>Airbnb</span>
+        </div>
       </div>
     </div>
   );
