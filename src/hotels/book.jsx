1febeb1a1b8599b63af087becf6d7e6b5fb72de9
MadeleneRose <katarinamadelene@hotmail.com>
2024-10-20 01:47:19 +0200

    book

--- src/hotels/book.jsx
+++ src/hotels/book.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css'; 
 import Header from '../Header.jsx'; 
 import YellowLine from '../YellowLine.jsx'; 
@@ -11,29 +11,51 @@ import HeadingBlock from '../HeadingBlock';
 import Footer from '../Footer'; 
 import Button from '../Button'; 
 
-
 const Booking = () => {
+  const [step, setStep] = useState('booking'); 
+
 
   const handleProceedClick = () => {
     console.log("Proceed with booking clicked");
+    setStep('confirm'); 
+  };
 
+
+  const handleConfirmClick = () => {
+    console.log("Confirm and pay clicked");
+ 
+ 
   };
 
   return (
     <div>
-      <Header /> 
+      <Header />
       <HeadingBlock text="Prices and booking" />
-      <YellowLine /> 
-      <ImageComponent src={imageOneAboutUs} alt="imageOneAboutUs" /> 
-      <TravelDetailsData /> 
-      <BookingOptionsForm />
-      <TotalPrice />
+      <YellowLine />
+      <ImageComponent src={imageOneAboutUs} alt="imageOneAboutUs" />
 
-      <Button type="button" className="proceed-button" onClick={handleProceedClick}>
-        Proceed with booking
-      </Button>
+      {step === 'booking' && (
+        <div>
+          <TravelDetailsData />
+          <BookingOptionsForm />
+          <TotalPrice />
+          <Button type="button" className="proceed-button" onClick={handleProceedClick}>
+            Proceed with booking
+          </Button>
+        </div>
+      )}
 
-      <Footer /> 
+      {step === 'confirm' && (
+        <div>
+          <TravelDetailsData />
+          <TotalPrice />
+          <Button type="button" className="confirm-button" onClick={handleConfirmClick}>
+            Confirm and pay
+          </Button>
+        </div>
+      )}
+
+      <Footer />
     </div>
   );
 };
